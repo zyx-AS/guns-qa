321e6a7ca397272b8dcbef0becbfbe57584668ab
止木 <1906286028@qq.com>
2026-05-04 16:22:08 +0800

    GUNSQA-123 add xray integration test

---
+++ guns-tests/src/test/java/cn/stylefeng/roses/kernel/sys/modular/user/service/impl/XrayIntegrationTest.java
@@ -0,0 +1,13 @@
+package cn.stylefeng.roses.kernel.sys.modular.user.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class XrayIntegrationTest {
+
+    @Test
+    void shouldPassXrayIntegrationValidation() {
+        assertTrue(true);
+    }
+}
