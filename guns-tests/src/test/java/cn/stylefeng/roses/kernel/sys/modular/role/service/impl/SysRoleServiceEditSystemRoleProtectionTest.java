515d6d18f8f4265944c43ad30f0457003fbdc963
刘晓铃 <302023315395@zjut.edu.cn>
2026-05-09 22:09:07 +0800

    GUNSQA-55 add system role protection cases

--- guns-tests/src/test/java/cn/stylefeng/roses/kernel/sys/modular/role/service/impl/SysRoleServiceEditSystemRoleProtectionTest.java
+++ guns-tests/src/test/java/cn/stylefeng/roses/kernel/sys/modular/role/service/impl/SysRoleServiceEditSystemRoleProtectionTest.java
@@ -7,14 +7,18 @@ import cn.stylefeng.roses.kernel.rule.exception.base.ServiceException;
 import cn.stylefeng.roses.kernel.sys.api.enums.role.RoleTypeEnum;
 import cn.stylefeng.roses.kernel.sys.modular.role.entity.SysRole;
 import cn.stylefeng.roses.kernel.sys.modular.role.pojo.request.SysRoleRequest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
@@ -34,23 +38,31 @@ class SysRoleServiceEditSystemRoleProtectionTest {
     @InjectMocks
     private SysRoleServiceImpl sysRoleService;
 
-    @Test
-    void shouldRejectDowngradingSystemRoleByForgingEditableFields() {
+    @ParameterizedTest
+    @MethodSource("systemRoleEditScenarios")
+    void shouldRejectDowngradingSystemRoleByForgingEditableFields(
+            Long roleId,
+            Integer requestRoleType,
+            Long requestRoleCompanyId,
+            Integer persistedRoleType,
+            Long persistedRoleCompanyId,
+            boolean shouldThrow
+    ) {
         Long currentCompanyId = 100L;
 
         SysRoleRequest request = new SysRoleRequest();
-        request.setRoleId(1L);
+        request.setRoleId(roleId);
         request.setRoleCode("system-admin");
-        request.setRoleType(RoleTypeEnum.COMPANY_ROLE.getCode());
-        request.setRoleCompanyId(currentCompanyId);
+        request.setRoleType(requestRoleType);
+        request.setRoleCompanyId(requestRoleCompanyId);
 
         SysRole persistedRole = new SysRole();
-        persistedRole.setRoleId(1L);
+        persistedRole.setRoleId(roleId);
         persistedRole.setRoleCode("system-admin");
-        persistedRole.setRoleType(RoleTypeEnum.SYSTEM_ROLE.getCode());
-        persistedRole.setRoleCompanyId(null);
+        persistedRole.setRoleType(persistedRoleType);
+        persistedRole.setRoleCompanyId(persistedRoleCompanyId);
 
-        doReturn(persistedRole).when(sysRoleService).getById(1L);
+        doReturn(persistedRole).when(sysRoleService).getById(roleId);
         doReturn(Boolean.TRUE).when(sysRoleService).updateById(any(SysRole.class));
 
         try (MockedStatic<LoginContext> loginContextMock = mockStatic(LoginContext.class)) {
@@ -58,7 +70,49 @@ class SysRoleServiceEditSystemRoleProtectionTest {
             when(loginUserApi.getSuperAdminFlag()).thenReturn(false);
             when(loginUserApi.getCurrentUserCompanyId()).thenReturn(currentCompanyId);
 
-            assertThrows(ServiceException.class, () -> sysRoleService.edit(request));
+            if (shouldThrow) {
+                assertThrows(ServiceException.class, () -> sysRoleService.edit(request));
+            } else {
+                assertDoesNotThrow(() -> sysRoleService.edit(request));
+            }
         }
+    }
+
+    private static Stream<org.junit.jupiter.params.provider.Arguments> systemRoleEditScenarios() {
+        Long currentCompanyId = 100L;
+        return Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of(
+                        1L,
+                        RoleTypeEnum.COMPANY_ROLE.getCode(),
+                        null,
+                        RoleTypeEnum.SYSTEM_ROLE.getCode(),
+                        null,
+                        true
+                ),
+                org.junit.jupiter.params.provider.Arguments.of(
+                        1L,
+                        RoleTypeEnum.SYSTEM_ROLE.getCode(),
+                        currentCompanyId,
+                        RoleTypeEnum.SYSTEM_ROLE.getCode(),
+                        null,
+                        true
+                ),
+                org.junit.jupiter.params.provider.Arguments.of(
+                        1L,
+                        RoleTypeEnum.COMPANY_ROLE.getCode(),
+                        currentCompanyId,
+                        RoleTypeEnum.SYSTEM_ROLE.getCode(),
+                        null,
+                        true
+                ),
+                org.junit.jupiter.params.provider.Arguments.of(
+                        10L,
+                        RoleTypeEnum.COMPANY_ROLE.getCode(),
+                        currentCompanyId,
+                        RoleTypeEnum.COMPANY_ROLE.getCode(),
+                        currentCompanyId,
+                        false
+                )
+        );
     }
 }
